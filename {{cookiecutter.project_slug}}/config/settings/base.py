345895f6c97d029aa6608d8e8ebcd29de9c1a5eb
geoanalytic <dcurrie@geoanalytic.com>
2018-04-30 12:17:54 -0600

    added django.contrib.gis

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -64,6 +64,7 @@ DJANGO_APPS = [
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     # 'django.contrib.humanize', # Handy template tags
     'django.contrib.admin',
 ]
