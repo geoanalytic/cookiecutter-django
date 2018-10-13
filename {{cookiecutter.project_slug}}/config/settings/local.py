fec57123252362bf661fb7bfd5a931e3b6ef6bb7
David Currie <hunter.currie@gmail.com>
2018-10-13 11:01:25 -0600

    changed ALLOWED_HOSTs to "*"

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='!!!SET DJANGO_SECRET_KEY!!!')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [*]   # changed to allow testing with mobile on local network
+ALLOWED_HOSTS = ["*",]   # changed to allow testing with mobile on local network
 #    "localhost",
 #    "0.0.0.0",
 #    "127.0.0.1",
